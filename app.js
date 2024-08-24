22d187ab6d8d713b177942ddcf31c8b3de2b452b
HassanDevoloper <161868607+HassanDevoloper@users.noreply.github.com>
2024-08-24 11:26:45 +0500

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ let userInp = prompt("Enter table name");
 
 for (var i = 1; i <= 10; i++) {
  let value =(userInp*i);
- document.write( userInp ,"x" ,i ,"=", value);
+ console.log( userInp ,"x" ,i ,"=", value);
 }
 
